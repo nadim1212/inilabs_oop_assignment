ce5b4e7b2779932fb1fef99f6334b3ad383d0823
nadim1212 <nadimniloy.swe@gmail.com>
2023-08-20 18:38:31 +0600

    polymorphism

--- polymorphism.php
+++ polymorphism.php
@@ -1,9 +1,34 @@
 <?php
+interface SoundMaker {
+    public function makeSound();
+}
 
+class Dog implements SoundMaker {
+    public function makeSound() {
+        return "Woof!";
+    }
+}
 
+class Cat implements SoundMaker {
+    public function makeSound() {
+        return "Meow!";
+    }
+}
 
+class Duck implements SoundMaker {
+    public function makeSound() {
+        return "Quack!";
+    }
+}
 
+$animals = [
+    new Dog(),
+    new Cat(),
+    new Duck()
+];
 
-
+foreach ($animals as $animal) {
+    echo get_class($animal) . " says: " . $animal->makeSound() . "\n";
+}
 
 ?>
